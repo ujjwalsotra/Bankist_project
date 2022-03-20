21ff5406466637d81046972e730db838a451830f
ujjwalsotra <69038984+ujjwalsotra@users.noreply.github.com>
2022-03-20 13:21:18 +0530

    Page Navigation developed.

--- script.js
+++ script.js
@@ -29,59 +29,7 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
-
-
-// SELECTING ELEMENTS
-
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
-
-const header=document.querySelector('.header');
-const allSelections=document.querySelectorAll('.section');
-console.log(allSelections); 
-
-document.getElementById('section--1');
-const allButtons=document.getElementsByTagName('button');
-console.log(allButtons);
-
-console.log(document.getElementsByClassName('btn'));
-
-//Creating and inserting elements
-
-const message= document.createElement('div');
-message.classList.add('cookie-message');
-//message.textContent='We use cookies for improved and better experience';
-message.innerHTML= 'We use cookies for improved and better experience. <button class="btn btn--close-cookie">Got it!</button>';
-//header.prepend(message);  // Prepend basically as the first child of header element
-header.append(message);  // Last chile of the header element (element on which method is called)
-//header.append(message.cloneNode(true));// Now i can show the same message at top and end of the element
-
-// Delete elements
-
-document.querySelector('.btn--close-cookie').addEventListener('click',function(){
-  message.remove();
-})
-
-// Styles
-
-message.style.backgroundColor='#37383d';
-message.style.width='120%';
-message.style.height=Number.parseFloat(getComputedStyle(message).height,10)+30+'px';
-//document.documentElement.style.setProperty('--color-primary','orangered');
-
-// Attributes
-
-const logo=document.querySelector('.nav__logo');
-console.log(logo.src);
-
-logo.alt='Beautiful minimalist logo';// Setting the attributes value
-
-// Data Attributes
-console.log(logo.dataset.versionNumber); // We use data attributes quite a lot when we work with API and has to store data in datasets.
-
-
-// IMPLEMENTING SMOOTH SCROLLING
+//IMPLEMENTING SMOOTH SCROLLING
 
 const btnScrollTo=document.querySelector('.btn--scroll-to');
 const section1=document.querySelector('#section--1');
@@ -94,27 +42,121 @@ btnScrollTo.addEventListener('click',function(e){
   section1.scrollIntoView({behavior: 'smooth'});
 });
 
+//------------------------------------
+// Page Navigation
+// In event delegation we use the fact that events bubble up
+//document.querySelectorAll('.nav__link').forEach(function(el){
+  //el.addEventListener('click',function(e){
+    //e.preventDefault();
+    //const id=this.getAttribute('href');
+    //console.log(id);
+    //document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+  //});
+//});
+
+
+//event delegation
+// step 1: add event listener to common parent element
+// step 2: determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click',function(e){
+  //console.log(e.target);
+
+  e.preventDefault();
+  // Matching strategy
+
+    if(e.target.classList.contains('nav__link'))
+    {
+      const id=e.target.getAttribute('href');
+      document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+    }
+
+});
+
+
+// SELECTING ELEMENTS
+//console.log(document.documentElement);
+//console.log(document.head);
+//console.log(document.body);
+
+//const header=document.querySelector('.header');
+//const allSelections=document.querySelectorAll('.section');
+//console.log(allSelections); 
+
+//document.getElementById('section--1');
+//const allButtons=document.getElementsByTagName('button');
+//console.log(allButtons);
+
+//console.log(document.getElementsByClassName('btn'));
+
+//Creating and inserting elements
+
+////const message= document.createElement('div');
+////message.classList.add('cookie-message');
+//message.textContent='We use cookies for improved and better experience';
+////message.innerHTML= 'We use cookies for improved and better experience. <button class="btn btn--close-cookie">Got it!</button>';
+//header.prepend(message);  // Prepend basically as the first child of header element
+////header.append(message);  // Last chile of the header element (element on which method is called)
+//header.append(message.cloneNode(true));// Now i can show the same message at top and end of the element
+
+// Delete elements
+
+////document.querySelector('.btn--close-cookie').addEventListener('click',function(){
+////  message.remove();
+////})
+
+// Styles
+
+////message.style.backgroundColor='#37383d';
+////message.style.width='120%';
+////message.style.height=Number.parseFloat(getComputedStyle(message).height,10)+30+'px';
+//document.documentElement.style.setProperty('--color-primary','orangered');
+
+// Attributes
+
+////const logo=document.querySelector('.nav__logo');
+////console.log(logo.src);
+
+////logo.alt='Beautiful minimalist logo';// Setting the attributes value
+
+// Data Attributes
+////console.log(logo.dataset.versionNumber); // We use data attributes quite a lot when we work with API and has to store data in datasets.
+
+
+// IMPLEMENTING SMOOTH SCROLLING
+
+//const btnScrollTo=document.querySelector('.btn--scroll-to');
+//const section1=document.querySelector('#section--1');
+
+//btnScrollTo.addEventListener('click',function(e){
+  //const s1coords=section1.getBoundingClientRect();
+  //console.log(s1coords);
+  //console.log(e.target.getBoundingClientRect()); 
+  //console.log('Current scroll (X/Y)',window.pageXOffset, window.pageYOffset);
+  //section1.scrollIntoView({behavior: 'smooth'});
+//});
+
 // Types of event and event handlers
 
 // An event is a type of signal that is genetared by the  ceratin DOM node.
 
-const h1=document.querySelector('h1');
+//const h1=document.querySelector('h1');
 
-h1.addEventListener('mouseenter',function(e){ // Working on mouse enter event  , When we hover over 
-  alert('addEventListener jai mata di');
+//h1.addEventListener('mouseenter',function(e){ // Working on mouse enter event  , When we hover over 
+  //alert('addEventListener jai mata di');
 
    
-});
+//});
 
 // Event propogation : Bubbling and capturing
 
-const randomInt=(min,max)=>Math.floor(Math.random()*(max-min+1)+min);
-const randomColor = ()=> `rgb(${randomInt(0,255)}),rgb(${randomInt(0,255)}),rgb(${randomInt(0,255)})`;
+//const randomInt=(min,max)=>Math.floor(Math.random()*(max-min+1)+min);
+//const randomColor = ()=> `rgb(${randomInt(0,255)}),rgb(${randomInt(0,255)}),rgb(${randomInt(0,255)})`;
 //console.log(randomColor(0,255));
 
-document.querySelector('.nav__link').addEventListener('click',function(e){
-  this.style.backgroundColor=randomColor();
-});
+//document.querySelector('.nav__link').addEventListener('click',function(e){
+  //this.style.backgroundColor=randomColor();
+//});
 
 //document.querySelector('.nav__links').addEventListener('click',function(e){
   //console.log('LINK');
